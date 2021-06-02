7ff1cd157680d9fb05f38403c3150e80fc8b01a9
Ishan Kumar <itsik159@gmail.com>
2021-06-02 22:51:17 +0530

    tired of minor changes

--- api/index.js
+++ api/index.js
@@ -5,13 +5,30 @@ require('dotenv').config();
 const cors = require('cors')
 
 
-
-mongoose.connect(process.env.MONGO_URI ,
+async function connectMongo() {
+	try {
+		await mongoose.connect(
+			process.env.MONGO_URI,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			},
+			(err) => {
+				if (err) console.log(err);
+				else console.log('Connected to MongoDB');
+			}
+		);
+	} catch (e) {
+		console.log(e);
+	}
+}
+connectMongo();
+/*mongoose.connect(process.env.MONGO_URI ,
     
     { useNewUrlParser:true , useUnifiedTopology: true , useCreateIndex:true
     }).then(() => {
         console.log("db  conncted")
-    }).catch((err) => {console.log("db not connected" + err)})
+    }).catch((err) => {console.log("db not connected" + err)})*/
 
     app.use(cors())
     app.use(express.json());
